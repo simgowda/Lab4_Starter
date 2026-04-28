1d4d2c0d45c5261d28ba6aa3d2a13597d0c44ea9
simgowda <simargowda@gmail.com>
2026-04-28 16:33:45 -0700

    fixed scripts FINAL w/edge cases

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,9 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return false;
+    }
     if (add) {
         //changed from const to let
         let result = 0;
@@ -26,9 +29,15 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || prices.length === 0 || typeof discount !== "number") {
+        return false;
+    }
     const discounted = []
 
     for(let i = 0; i < prices.length; i++) {
+        if (typeof prices[i] !== "number") {
+            return false;
+        }
         const discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
